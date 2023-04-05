f6fc98baf4e85cfc586b31794f784104e34002c5
1nKokoreva <s22136@pjwstk.edu.pl>
2023-04-05 22:17:51 +0200

    Add new feature to code

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using Zadanie4.DTOs;
 
@@ -12,41 +11,110 @@ namespace Zadanie4.Controllers
         [HttpGet]
         public ActionResult Get([FromQuery]string param)
         {
-            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql16;Initial Catalog=sXXXXX;Integrated Security=True"))
+            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql16;Initial Catalog=s22136;Integrated Security=True"))
             {
                 connection.Open();
-
-                using SqlCommand command = new SqlCommand($"SELECT * FROM... {param}", connection);
+                using SqlCommand command = new SqlCommand($"SELECT * FROM Animal ORDER BY {param}", connection);
 
                 SqlDataReader reader = command.ExecuteReader();
-
+                //List of animals
+                List<AnimalDTO> animalDtos = new List<AnimalDTO>();
                 while (reader.Read())
                 {
-                    int id = (int)reader["..."];
-
+                    var id = (int)reader["IdAnimal"];
+                    var name = (string)reader["Name"];
+                    var description = (string)reader["Description"];
+                    var category = (string)reader["Category"];
+                    var area = (string)reader["Area"];
+                    animalDtos.Add(new AnimalDTO(id, name, description, category, area));
                 }
-
+                
                 reader.Close();
                 connection.Close();
 
-                return Ok();
+                if (animalDtos.Count > 0)
+                {
+                  //  var jsonString = Json.Encode(animalDtos);
+                    return Ok(animalDtos);
+                }
+                else
+                {
+                    return BadRequest("Not enough data");
+                }
             }
         }
 
-        [HttpPost]
-        public ActionResult Post(AnimalDTO animal)
+
+        [HttpPut]
+        public IActionResult UpdateAnimal(int id, [FromBody] AnimalDTO updatedAnimal)
         {
-            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql16;Initial Catalog=sXXXXX;Integrated Security=True"))
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql16;Initial Catalog=s22136;Integrated Security=True"))
             {
                 connection.Open();
+                string query = $"UPDATE Animal SET Name = '{updatedAnimal.Name}', Description = '{updatedAnimal.Description}', Category = '{updatedAnimal.Category}', Area = '{updatedAnimal.Area}' WHERE IdAnimal = {id}";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+        }
 
-                using SqlCommand command = new SqlCommand("INSERT INTO ... VALUES (@Name...", connection);
 
-                command.Parameters.AddWithValue("@Name", "...");
+        [HttpDelete]
+        public ActionResult DeletAnimal(int id)
+        {
+            using (SqlConnection connection =
+                   new SqlConnection("Data Source=db-mssql16;Initial Catalog=s22136;Integrated Security=True"))
+            {
+                connection.Open();
+                using SqlCommand command = new SqlCommand($"DELETE FROM Animal WHERE IdAnimal = {id}", connection);
 
                 var numberOfRows = (int?)command.ExecuteNonQuery();
                 connection.Close();
 
+                if (numberOfRows == 0)
+                {
+                    return NotFound();
+                }
+                
+            }
+
+            return Ok("animal was deleted");
+        }
+        
+
+        [HttpPost]
+        public ActionResult Post(AnimalDTO animal)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=db-mssql16;Initial Catalog=s22136;Integrated Security=True"))
+            {
+                connection.Open();
+
+                using SqlCommand command = new SqlCommand("INSERT INTO Animal VALUES (@Name, @Description, @Category, @Area)", connection);
+
+                command.Parameters.AddWithValue("@Name", animal.Name);
+                command.Parameters.AddWithValue("@Description", animal.Description);
+                command.Parameters.AddWithValue("@Category", animal.Category);
+                command.Parameters.AddWithValue("@Area", animal.Area);
+                
+                var numberOfRows = (int?)command.ExecuteNonQuery();
+                connection.Close();
+                if (numberOfRows == 0)
+                {
+                    return NotFound();
+                }
                 return Ok();
             }
         }
--- DTOs/AnimalDTO.cs
+++ DTOs/AnimalDTO.cs
@@ -2,6 +2,20 @@
 {
     public class AnimalDTO
     {
+        public AnimalDTO(int index, string name, string description, string category, string area)
+        {
+            Index = index;
+            Name = name;
+            Description = description;
+            Category = category;
+            Area = area;
+        }
+
+        public int Index { get; set; } 
         public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Area { get; set; }
+        
     }
 }
